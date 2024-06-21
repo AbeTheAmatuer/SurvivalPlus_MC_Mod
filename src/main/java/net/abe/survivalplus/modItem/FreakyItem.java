ac4aebfd5843a9272feabfca127d020beee62338
Sir Abe <ibrahimkhurram2008@gmail.com>
2024-06-22 02:50:15 +0500

    added random effect roll when using freaky item

--- src/main/java/net/abe/survivalplus/modItem/FreakyItem.java
+++ src/main/java/net/abe/survivalplus/modItem/FreakyItem.java
@@ -15,14 +15,15 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 import org.w3c.dom.Text;
-
+import java.util.*;
 import static org.w3c.dom.Text.*;
 
 public class FreakyItem extends Item {
     public FreakyItem(Settings settings) {
         super(settings);
-    }
 
+    }
+    public static Random rand = new Random();
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         user.playSound(SoundEvents.BLOCK_HONEY_BLOCK_SLIDE, 2.0F, 1.0F);
@@ -30,30 +31,21 @@ public class FreakyItem extends Item {
         world.addParticle(ParticleTypes.GUST, true, user.getX() - 3, user.getY(), user.getZ(), 0.05, 0.05, 0.05);
         world.addParticle(ParticleTypes.GUST, true, user.getX(), user.getY(), user.getZ() + 4, 0.05, 0.05, -0.05);
         world.addParticle(ParticleTypes.SQUID_INK, true, user.getX(), user.getY(), user.getZ() - 3, 0.05, 0.05, 0.05);
-        world.addParticle(ParticleTypes.GUST, true, user.getX() + 2, user.getY()+1, user.getZ(), 0.05, -0.05, -0.05);
-        world.addParticle(ParticleTypes.GUST, true, user.getX() - 4, user.getY()+1, user.getZ(), 0.05, 0.05, 0.05);
-        world.addParticle(ParticleTypes.GUST, true, user.getX(), user.getY()+1, user.getZ() + 3, -0.05, 0.05, -0.05);
-        world.addParticle(ParticleTypes.GUST, true, user.getX(), user.getY()+2, user.getZ() - 4, 0.05, -0.05, 0.05);
-        world.addParticle(ParticleTypes.WARPED_SPORE, true, user.getX() + 1, user.getY(), user.getZ(), 0.05, 0.05, -0.05);
-        world.addParticle(ParticleTypes.GUST, true, user.getX() - 2, user.getY(), user.getZ(), 0.05, 0.05, 0.05);
-        world.addParticle(ParticleTypes.SOUL_FIRE_FLAME, true, user.getX(), user.getY(), user.getZ() + 2, 0.05, 0.05, -0.05);
-        world.addParticle(ParticleTypes.ANGRY_VILLAGER, true, user.getX(), user.getY(), user.getZ() - 4, 0.05, 0.05, 0.05);
-        world.addParticle(ParticleTypes.GUST, true, user.getX() + 3, user.getY()+1, user.getZ(), 0.05, -0.05, -0.05);
-        world.addParticle(ParticleTypes.WHITE_ASH, true, user.getX() - 1, user.getY()+1, user.getZ(), 0.05, 0.05, 0.05);
-        world.addParticle(ParticleTypes.SONIC_BOOM, true, user.getX(), user.getY()+1, user.getZ() + 3, -0.05, 0.05, -0.05);
-        world.addParticle(ParticleTypes.SQUID_INK, true, user.getX(), user.getY()+1, user.getZ() - 4, 0.05, -0.05, 0.05);
         //user.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 400, 400));
         user.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 400, 400));
         user.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 400, 20));
         user.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 400, 2));
         user.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 400, 2));
         user.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 150, 1));
-        user.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 30, 400));
         user.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAVING, 400, 3));
 
-
-
-
+        //Random Effect
+        int randNum = rand.nextInt(26);
+        if(randNum >= 15){
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 100, 4));
+        }else{
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 100, 2));
+        }
         return TypedActionResult.success(user.getStackInHand(hand));
     }
 }
