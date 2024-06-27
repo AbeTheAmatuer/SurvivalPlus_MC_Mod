842ec4b61541edcfabcf600514ca4c7aa8401cf1
Sir Abe <ibrahimkhurram2008@gmail.com>
2024-06-27 16:17:39 +0500

    ff

--- src/main/java/net/abe/survivalplus/modItem/FreakyItem.java
+++ src/main/java/net/abe/survivalplus/modItem/FreakyItem.java
@@ -21,9 +21,17 @@ import static org.w3c.dom.Text.*;
 public class FreakyItem extends Item {
     public FreakyItem(Settings settings) {
         super(settings);
+        randomEffects.add(new StatusEffectInstance(StatusEffects.NAUSEA, 100, 10));
+        randomEffects.add(new StatusEffectInstance(StatusEffects.REGENERATION, 100, 2));
+        randomEffects.add(new StatusEffectInstance(StatusEffects.LEVITATION, 100, 2));
+        randomEffects.add(new StatusEffectInstance(StatusEffects.HASTE, 100, 2));
+        randomEffects.add(new StatusEffectInstance(StatusEffects.TRIAL_OMEN, 10000, 5));
+        randomEffects.add(new StatusEffectInstance(StatusEffects.SLOWNESS, 100, 10));
 
     }
+    public static ArrayList<StatusEffectInstance> randomEffects = new ArrayList<StatusEffectInstance>();
     public static Random rand = new Random();
+
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         user.playSound(SoundEvents.BLOCK_HONEY_BLOCK_SLIDE, 2.0F, 1.0F);
@@ -31,22 +39,22 @@ public class FreakyItem extends Item {
         world.addParticle(ParticleTypes.GUST, true, user.getX() - 3, user.getY(), user.getZ(), 0.05, 0.05, 0.05);
         world.addParticle(ParticleTypes.GUST, true, user.getX(), user.getY(), user.getZ() + 4, 0.05, 0.05, -0.05);
         world.addParticle(ParticleTypes.SQUID_INK, true, user.getX(), user.getY(), user.getZ() - 3, 0.05, 0.05, 0.05);
-        //user.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 400, 400));
+        /*
         user.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 400, 400));
         user.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 400, 20));
         user.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 400, 2));
         user.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 400, 2));
         user.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 150, 1));
         user.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAVING, 400, 3));
-
+*/
         //Random Effect
-        int randNum = rand.nextInt(26);
-        if(randNum >= 15){
-            user.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 100, 4));
-        }else{
-            user.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 100, 2));
-        }
-        
+        int randNum = rand.nextInt(randomEffects.size());
+        System.out.println(randomEffects.get(randNum));
+        user.addStatusEffect(randomEffects.get(randNum));
+
+
+
+
         return TypedActionResult.success(user.getStackInHand(hand));
     }
 }
