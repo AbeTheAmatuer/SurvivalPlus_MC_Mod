ee0e557e9137dd303a6bea05bd52d0a3b7e36e29
Sir Abe <ibrahimkhurram2008@gmail.com>
2024-06-30 19:51:47 +0500

    m

--- src/main/java/net/abe/survivalplus/SurvivalPlus.java
+++ src/main/java/net/abe/survivalplus/SurvivalPlus.java
@@ -3,6 +3,7 @@ package net.abe.survivalplus;
 import net.abe.survivalplus.modItem.FreakyItem;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -16,12 +17,16 @@ public class SurvivalPlus implements ModInitializer {
 	public static final String MOD_ID = "survivalplus";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	//custom items
-	//public static final Item FREAKY_ITEM = new Item(new Item.Settings());
+	//Custom items
+
 	public static final FreakyItem FREAKY_ITEM = new FreakyItem(new Item.Settings());
+
+	//Custom blocks
+	public static final Block FREAKY_BLOCK = new Block(Block.Settings.create().strength(4.0f));
 	@Override
 	public void onInitialize() {
 		Registry.register(Registries.ITEM, Identifier.of("survivalplus", "freaky_item"), FREAKY_ITEM);
 
+		Registry.register(Registries.BLOCK, Identifier.of("suvivalplus", "freaky_block"), FREAKY_BLOCK);
 	}
-}
+} 
